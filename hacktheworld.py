b6548a774358d39a15c0cacd24ea8fc0a4b0f097
Ethan Ocasio <ewocasio@gmail.com>
2019-05-03 21:10:09 -0400

    match via UMLS

--- hacktheworld.py
+++ hacktheworld.py
@@ -1,6 +1,8 @@
 import patient as pt
 import importlib
 importlib.reload(pt)
+import logging
+import sys
 import umls
 import requests as req
 
@@ -8,6 +10,7 @@ import json
 
 class Patient:
     def __init__(self, sub, pat_token):
+#        logging.getLogger().setLevel(logging.DEBUG)
         self.sub = sub
         self.mrn = pat_token["mrn"]
         self.token = pat_token["token"]
@@ -21,11 +24,17 @@ class Patient:
 
     def load_codes(self):
         self.codes, self.names = pt.find_all_codes(self.conditions)
+        self.codes_ncit = []
+        for code_snomed in self.codes_snomed:
+            code_ncit = self.snomed2ncit(code_snomed)
+            if not (code_ncit == "no code match"):
+                self.codes_ncit.append(code_ncit)
         return
 
     def find_trials(self):
         self.trials = []
-        trials_json = pt.find_trials(self.codes)
+#        trials_json = pt.find_trials(self.codes)
+        trials_json = pt.find_trials(self.codes_ncit)
         for trialset in trials_json:
             for trial_json in trialset["trials"]:
                 trial = Trial(trial_json)
@@ -48,14 +57,20 @@ class Patient:
         return
 
     def snomed2ncit(self,code_snomed):
+        condition = self.conditions[self.codes_snomed.index(code_snomed)]
         tik = self.auth.getst(self.tgt)
         params = {"targetSource":"NCI","ticket":tik}
         res = req.get("https://uts-ws.nlm.nih.gov/rest/crosswalk/current/source/SNOMEDCT_US/"+code_snomed,params = params)
         if (res.status_code != 200):
+            logging.info("SNOMED CODE {} ({}) --> NO MATCH".format(code_snomed, condition))
             return "no code match"
         for result in res.json()["result"]:
             if not (result["ui"] in ["TCGA", "OMFAQ"]): 
-                return result["ui"]
+                name_ncit = result["name"]
+                code_ncit = result["ui"]
+                logging.info("SNOMED CODE {} ({})---> NCIT CODE {} ({})".format(code_snomed, condition, code_ncit, name_ncit))
+                logging.debug("SNOMED CODE {} JSON: {}".format(code_snomed, res.json()))
+                return code_ncit
         return "no code match"
 
 class PatientLoader:
