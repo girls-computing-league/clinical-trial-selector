e7f109697d3e6e2098d1238e03c427ec95578274
Ethan <ewocasio@gmail.com>
2020-03-03 17:02:56 -0500

    move socketio script to static file

---
+++ static/socket.js
@@ -0,0 +1,14 @@
+var socket = io();
+
+function openSocket() {
+    document.getElementById("progress").style.display = "block";
+    socket.on('update_progress', function(message) {
+        $('.progress-bar-inner').css('width', message.data+'%')
+                                .attr('aria-valuenow', message.data);
+        $('.progress-bar-label').text(message.data+'%');
+    });
+};
+
+socket.on('disconnect', function(message) {
+    socket.disconnect()
+});
