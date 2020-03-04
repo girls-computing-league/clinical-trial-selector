b3ebc040ca668eafefe16863bc4150dc121653d1
Ethan Ocasio <ewocasio@gmail.com>
2020-03-03 20:50:28 -0500

    onclick openSocket via jQuery

--- static/socket.js
+++ static/socket.js
@@ -1,5 +1,7 @@
 var socket = io();
 
+$("a[href=\"/getInfo\"]").click(openSocket)
+
 function openSocket() {
     document.getElementById("progress").style.display = "block";
     socket.on('update_progress', function(message) {
