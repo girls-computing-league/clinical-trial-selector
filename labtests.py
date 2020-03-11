90cbc1017c8049e61baa5f533e08844e53e5fbce
Ethan <ewocasio@gmail.com>
2020-03-11 14:40:01 -0400

    start regex creation

--- labtests.py
+++ labtests.py
@@ -1,3 +1,5 @@
+import re
+
 class LabTest:
 
     def __init__(self, name: str, aliases=[], loincs=[], units=[]):
@@ -9,20 +11,30 @@ class LabTest:
 
 class labs:
     tests = [
-        LabTest("hemoglobin", aliases=["hgb"], loincs=["1234"], units=["mg/dl"]),
-        LabTest("platelets", aliases=["plt"], loincs=["2345"], units=["cells/microliter"])
+        LabTest("hemoglobin", 
+            aliases=["hgb"], 
+            loincs=["718-7"], 
+            units=["mg/dl"]),
+        LabTest("platelets", 
+            aliases=["plt", "platelet count"], 
+            loincs=["777-3"], 
+            units=["cells/microliter"])
     ]
     by_name = {}
     by_alias = {}
     by_loinc = {}
+    alias_regex = {}
 
     @classmethod
     def create_maps(cls):
+        all_aliases = []
         for test in cls.tests:
             cls.by_name[test.name] = test
+            all_aliases.extend(test.aliases)
             for alias in test.aliases:
                 cls.by_alias[alias] = test
             for loinc in test.loincs:
                 cls.by_loinc[loinc] = test
-
+        cls.alias_regex = re.compile(f"({'|'.join(all_aliases)})")
+    
 labs.create_maps()
