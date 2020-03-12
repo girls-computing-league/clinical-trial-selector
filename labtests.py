d5b2d42b241cc80f87e2448d5ea5d90f86d0fa46
Ethan <ewocasio@gmail.com>
2020-03-11 21:06:39 -0400

    create regex function

--- labtests.py
+++ labtests.py
@@ -26,22 +26,27 @@ class labs:
     alias_regex = {}
     criteria_regex = {}
 
+    _all_aliases = []
+
     @classmethod
     def create_maps(cls):
-        all_aliases = []
         for test in cls.tests:
             cls.by_name[test.name] = test
-            all_aliases.extend(test.aliases)
+            _all_aliases.extend(test.aliases)
             for alias in test.aliases:
                 cls.by_alias[alias] = test
             for loinc in test.loincs:
                 cls.by_loinc[loinc] = test
-        alias_pattern  = f"({'|'.join(all_aliases)})"
+
+    @classmethod
+    def create_regext(cls):
+        alias_pattern  = f"({'|'.join(_all_aliases)})"
         cls.alias_regex = re.compile(alias_pattern, re.IGNORECASE)
         abbreviation_pattern = "(?:\(\w+\))?"
-        compare_pattern = "(<|<=|=|>=|>)"
+        compare_pattern = "(<|<=|=|>=|>|≥)"
         number_pattern = "(\d+(?:,\d{3})*(?:.\d*)?)"
         combined_pattern = "\s*".join([alias_pattern, abbreviation_pattern, compare_pattern, number_pattern])
         cls.criteria_regex = re.compile(combined_pattern, re.IGNORECASE)
     
 labs.create_maps()
+labs.create_regex()
